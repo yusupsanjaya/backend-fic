c836b31c72411557f3dde697155e6decdbfd9a66
yusup sanjaya <yusupsanjaya8@gmail.com>
2023-07-16 11:19:51 +0700

    backend-fic6

---
+++ src/api/payment-callback/controllers/payment-callback.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * payment-callback controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payment-callback.payment-callback', ({ strapi }) => ({
+    async create(ctx) {
+        let requestData = ctx.request.body;
+        console.log('request: ', requestData);
+
+        let order = await strapi.service('api::order.order').findOne(requestData.order_id);
+        let inputData = { 'data': { 'history': requestData } };
+
+        const result = await strapi.service('api::payment-callback.payment-callback').create(inputData);
+
+        let params = {}
+
+        if (requestData.transaction_status == 'settlement') {
+            params = { 'data': { 'statusOrder': 'purchased' } }
+        } else {
+            params = { 'data': { 'statusOrder': 'cancel' } }
+        }
+
+        console.log('id: ', requestData.order_id);
+        let updateOrder = await strapi.service('api::order.order').update(requestData.order_id, params);
+
+        console.log('update data: ', updateOrder);
+
+        return { 'data': updateOrder }
+    }
+}));
